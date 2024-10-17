4a3563af264ed8d7d2783b1edfa3af4d2d2792ec
Paul Mallet <pamallet@c1r7p10.42lehavre.fr>
2024-10-17 19:07:50 +0200

    Added all functions

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,29 +6,47 @@
 /*   By: pamallet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:11:07 by pamallet          #+#    #+#             */
-/*   Updated: 2024/10/17 16:20:18 by pamallet         ###   ########.fr       */
+/*   Updated: 2024/10/17 19:06:51 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 #include "libft.h"
-/*
-void	*ft_memcpy(void *dest, const void src, size_t n)
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	//
+	size_t	i;
+	char	*dst;
+	char	*sc;
+
+	i = 0;
+	dst = (char *)dest;
+	sc = (char *)src;
+	if (ft_strlen(dest) < n)
+	{
+		while (sc[i]) //i < n
+		{
+			dst[i] = (char)sc[i];
+			i++;
+		}
+	}
+	dst[i] = '\0';
+	return (dst);
 }
-*/
+
 int	main(void)
 {
-	char	dest[50] = "test"; //4
-	char	*src = "de test"; //7
+	char	dest1[50] = "test"; //4
+	char	dest2[50] = "test"; //4
+	char	*src = "okok"; //4
 
-	printf("before: %s\n", dest);
-	//ft_memcpy(dest, src, 3));
-	memcpy(dest, src, ); //overwrite || end at src[i] = '\0'
+	printf("before: %s\n", dest1);
+	ft_memcpy(dest1, src, 5 * sizeof(char));
+	printf("after: %s\n", dest1);
 
-	printf("after: %s\n", dest);
+	printf("before: %s\n", dest2);
+	memcpy(dest2, src, 5 * sizeof(char)); //overwrite || end at src[i] = '\0'
+	printf("after: %s\n", dest2);
 	return (0);
 }
-
