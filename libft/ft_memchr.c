cbc4398d7bdb0fcefb0230a00253e235feab6285
paul_mallet <paulhavre42@gmail.com>
2024-10-20 12:49:51 +0200

    Added all fd functions

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,17 +1,23 @@
 
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
 
-
-void	*ft_memchr(void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	//
 }
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	void	*s;
+	char	*str;
 
-	printf("\n", ft_memchr(s, 'a', 3));
-	printf("\n", memchr(s, 'a', 3));
+	if (ac == 2)
+	{
+		//printf("\n", ft_memchr("test", 'a', 3 * sizeof(char)));
+		str = memchr(av[1], 'a', 4);
+		printf("%s\n", str);
+	}
 	return (0);
 }
 
