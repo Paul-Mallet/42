6190dd67650fd30591630c85c3eb70d8234dc531
Paul Mallet <pamallet@c1r7p9.42lehavre.fr>
2024-10-25 14:33:51 +0200

    Added all bonus functions

--- libft/ft_lstadd_front_bonus.c
+++ libft/ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: pamallet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:41:41 by pamallet          #+#    #+#             */
-/*   Updated: 2024/10/24 18:54:20 by pamallet         ###   ########.fr       */
+/*   Updated: 2024/10/25 11:47:46 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,27 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	lst->next = new;
+	if (new != NULL)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
 }
-
+/*
 int	main(int ac, char **av)
 {
+	t_list	*lst;
+	t_list	*new;
+
+	lst = ft_lstnew("old");
+	new = ft_lstnew("new");
 	(void)ac;
 	(void)av;
 	//if (ac == 2)
 	//{
-		ft_lstadd_front(&ft_lstnew("1rst"), ft_lstnew("2nd"));
+		ft_lstadd_front(&lst, new);
+		printf("%s\n", (char *)lst->next->content);
 	//}
 	return (0);
 }
+*/
--- libft/ft_lstsize_bonus.c
+++ libft/ft_lstsize_bonus.c
@@ -1,13 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pamallet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:41:58 by pamallet          #+#    #+#             */
-/*   Updated: 2024/10/24 14:41:59 by pamallet         ###   ########.fr       */
+/*   Updated: 2024/10/25 12:35:50 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_lstsize(t_list *lst)
+{
+	int	c;
+
+	c = 0;
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		c++;
+	}
+	return (c);
+}
+/*
+int	main(int ac, char **av)
+{
+	t_list	*lst;
+	t_list	*l2;
+
+	lst = ft_lstnew("1");
+	l2 = ft_lstnew("2");
+
+	ft_lstadd_front(&lst, l2);
+	(void)ac;
+	(void)av;
+	//if (ac == 2)
+	//{
+		printf("%d\n", ft_lstsize(lst));
+	//}
+	//else
+	//	printf("Need valid arguments!");
+	return (0);
+}
+*/
