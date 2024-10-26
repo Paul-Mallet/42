53c8980a889eb76fd8bf1ae941191d65eab82b40
paul_mallet <paulhavre42@gmail.com>
2024-10-27 00:17:02 +0200

    Added all bonus functions

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*curr;
+	t_list	*temp;
+
+	curr = *lst;
+	while (curr != NULL)
+	{
+		temp = curr->next;
+		ft_lstdelone(curr, del);
+		curr = temp;
+	}
+	curr = NULL;
+}
+/*
+int	main(int ac, char **av)
+{
+	t_list	*head;
+	t_list	*mid;
+	t_list	*tail;
+
+	head = ft_lstnew(av[1]);
+	mid = ft_lstnew(av[2]);
+	tail = ft_lstnew(av[3]);
+	ft_lstadd_back(&head, mid);
+	ft_lstadd_back(&head, tail); //head->mid->tail
+	ft_lstclear(&head, ft_lstdel); //head->null
+	if (ac == 4)
+		printf("%s\n", (char *)ft_lstlast(head)->content);
+	else
+		printf("Invalid arguments!");
+	return (0);
+}
+*/
