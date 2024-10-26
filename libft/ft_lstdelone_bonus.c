122d95706d75ca96b68f0830e000647fb6b952ba
paul_mallet <paulhavre42@gmail.com>
2024-10-26 23:43:08 +0200

    Added all bonus functions

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -10,4 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+/*
+static void	ft_lstdel(void *content)
+{
+	free(content);
+}
+*/
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst != NULL && del != NULL)
+	{
+		(*del)(lst->content);
+		free(lst);
+	}
+}
+/*
+int	main(int ac, char **av)
+{
+	t_list	*head;
+	t_list	*mid;
+	t_list	*tail;
+
+	head = ft_lstnew(av[1]);
+	mid = ft_lstnew(av[2]);
+	tail = ft_lstnew(av[3]);
+	ft_lstadd_back(&head, mid);
+	ft_lstadd_back(&head, tail);
+	ft_lstdelone(tail, ft_lstdel);
+	if (ac == 5)
+		printf("%s\n", (char *)ft_lstlast(head)->content);
+	else
+		printf("Invalid arguments!");
+	return (0);
+}
+*/
