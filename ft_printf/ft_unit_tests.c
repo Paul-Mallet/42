1ba612ce5d6be2756824633b76555384dd654729
Paul Mallet <pamallet@c1r6p12.42lehavre.fr>
2024-11-07 16:28:59 +0100

    Added all ft_printf functions

--- ft_printf/ft_unit_tests.c
+++ ft_printf/ft_unit_tests.c
@@ -6,7 +6,7 @@
 /*   By: pamallet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 13:36:23 by pamallet          #+#    #+#             */
-/*   Updated: 2024/11/07 16:16:17 by pamallet         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:28:52 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,11 @@
 /* 	printf("i: %i\n", 42); */
 /* 	ft_printf("u: %u\n", 4294967295u); */
 /* 	printf("u: %u\n", 4294967295u); */
-/* 	ft_printf("x: %x\n", 255); */
-/* 	printf("x: %x\n", 255); */
+/* 	ft_printf("x: %x\n", -1); */
+/* 	printf("x: %x\n", -1); */
 /* 	ft_printf("X: %X\n", 255); */
 /* 	printf("X: %X\n", 255); */
-/* 	ft_printf("%: %%\n"); */
-/* 	printf("%: %%\n"); */
+/* 	ft_printf("prct: %%\n"); */
+/* 	printf("prct: %%\n"); */
 /* 	return (0); */
 /* } */
