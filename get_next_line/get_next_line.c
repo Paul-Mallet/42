b0498741439f982785893beb58d48157b319583a
paul_mallet <paulhavre42@gmail.com>
2024-12-04 11:11:44 +0100

    Added gnl main code

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,42 +6,76 @@
 /*   By: pamallet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:58:59 by pamallet          #+#    #+#             */
-/*   Updated: 2024/11/30 17:21:50 by pamallet         ###   ########.fr       */
+/*   Updated: 2024/12/03 13:30:50 by paul_mall        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*next_line(char *buf)//,len?
 {
-	static char	buf[BUFFER_SIZE + 1]; //static, 1 + 1 +...
-	int		size;
-
-	size = read(fd, buf, BUFFER_SIZE); //13
-	printf("size: %d\n", size);
-	buf[size] = '\0';
-
-	return (buf);
+	//implement strchr(), strjoin()
 }
 
-int	main(void)
+char	*read_line(int fd, char *buf)
 {
-	int	fd;
-	int	cl;
+	char		*line;
+	ssize_t		size;
+
+	size = read(fd, buf, BUFFER_SIZE);
+	if (size <= 0)
+	{
+		free(buf);
+		return (NULL);
+	}
+	buf[size] = '\0';
+	printf("%s\n", buf);
+	//buf = "My\nfirst\0"[8 + 1]
+	//line = "My\n"
+	line = next_line(buf);//len = 3, buf = 8 + 1
+	return (line); //line
+}
+//free buf OK, not line...
+char	*get_next_line(int fd)
+{
+	static char	*buf;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE < 0) //here + B_S < 1?
+		return (NULL);
+	//if (buf)
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	line = read_line(fd, buf);
+	return (line);
+}
+
+int	main(int ac, char **av)
+{
+	int		fd;
+	int		cl;
 	char	*line;
 
-	fd = open("line.txt", O_RDONLY);
-	if (fd >= 0)
-		printf("Fd opened: %d\n", fd);
-
-	while (get_next_line(fd))
+	(void)ac;
+	if (av[1])
+		fd = open(av[1], O_RDONLY);
+	else
+		fd = 0;
+	while (1)
 	{
 		line = get_next_line(fd);
-		printf("%s\n", line);
-	//free(line);
+		if (!line)
+		{
+			printf("(null)");
+			break ;
+		}
+		printf("%s", line);
+		free(line);
+		line = NULL;
 	}
 	cl = close(fd);
-	if (cl >= 0)
-		printf("Fd closed: %d\n", cl);
+	if (cl < 0)
+		printf("Close error!\n");
 	return (0);
 }
