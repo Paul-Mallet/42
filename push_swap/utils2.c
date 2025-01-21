5e182c521fca957a4afde8d0c8a5444e70ec875a
Paul Mallet <pamallet@c1r3p7.42lehavre.fr>
2025-01-21 16:27:27 +0100

    Added int overflows handlers

--- push_swap/utils2.c
+++ push_swap/utils2.c
@@ -6,21 +6,25 @@
 /*   By: paul_mallet <marvin@42.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 22:55:20 by paul_mall         #+#    #+#             */
-/*   Updated: 2025/01/21 14:15:06 by paul_mall        ###   ########.fr       */
+/*   Updated: 2025/01/21 16:25:49 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-//malloc? -> https://www.geeksforgeeks.org/check-for-integer-overflow/
-int	is_overflow(int a, int b)
+int	is_overflow(int a, int b, int c)
 {
-	return (a > 0 && (a + b < 0));
+	if (a > 0 && ((a * b + c) < 0))
+		return (1);
+	return (0);
 }
 
-int	is_underflow(int a, int b)
+int	is_underflow(int a, int b, int c)
 {
-	return (a < 0 && (a + b > 0));
+	printf("%d\n", a);
+	if (a < 0 && ((a * b + c) > 0))
+		return (1);
+	return (0);
 }
 
 int	ft_atoi(char *s)
@@ -37,9 +41,9 @@ int	ft_atoi(char *s)
 	}
 	while (*s >= '0' && *s <= '9')
 	{
-		if (is_overflow((res * 10) + (*s - 48), 1))
+		if (is_overflow(res, 10, (*s - 48)))
 			handle_error(ERROR_MSG);
-		if (is_underflow((res * 10) + (*s - 48), -1))
+		if (is_underflow((res * -1), 10, (*s - 48))) //not - yet
 			handle_error(ERROR_MSG);
 		res = (res * 10) + (*s++ - 48);
 	}
@@ -59,13 +63,3 @@ int	is_duplicate(t_stack *stk, int n)
 	}
 	return (0);
 }
-
-/* int	ft_quick_select(int *arr, int left, int right, int k) //median pivot */
-/* { */
-/* 	// */
-/* } */
-
-/* int	ft_hoare_partition(int *arr, int low, int high) //last index, smaller side */
-/* { */
-/* 	// */
-/* } */
---
+++ push_swap/utils_sort.c
@@ -0,0 +1,12 @@
+#include "push_swap.h"
+
+/* int	ft_quick_select(int *arr, int left, int right, int k) //median pivot */
+/* { */
+/* 	// */
+/* } */
+
+/* int	ft_hoare_partition(int *arr, int low, int high) //last index, smaller side */
+/* { */
+/* 	// */
+/* } */
+
