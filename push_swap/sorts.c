9ff06a1e0c90ae2bc20e56aaac59c218ba0fdaff
paul_mallet <paulhavre42@gmail.com>
2025-01-29 23:05:44 +0100

    Added first opti sort from a to b

--- push_swap/sorts.c
+++ push_swap/sorts.c
@@ -6,13 +6,13 @@
 /*   By: pamallet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 16:29:51 by pamallet          #+#    #+#             */
-/*   Updated: 2025/01/29 18:46:02 by pamallet         ###   ########.fr       */
+/*   Updated: 2025/01/29 22:58:32 by paul_mall        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	get_index(int elem, t_stack *stk) //a->arr[0], *b
+int	short_index(int elem, t_stack *stk)
 {
 	int	i;
 	int	save;
@@ -43,51 +43,74 @@ int	desc_sort_count(int elem, t_stack *stk)
 	int	short_elem;
 
 	nb_op = 0;
-	short_elem = stk->arr[get_index(elem, stk)];
+	short_elem = stk->arr[short_index(elem, stk)];
 	while (stk->arr[0] != short_elem)
-	{
-		if (get_index(elem, stk) < (stk->len / 2)) //pas call ops, just count!
-			rotate(stk);
-		else
-			rev_rotate(stk);
 		nb_op++;
-	}
 	return (nb_op);
 }
 
-void	cheap_sort(t_data *data)
+int	cheapest_index(t_data *data)
 {
 	int	i;
 	int	save;
+	int	save_i;
 	int	count;
 
 	i = 0;
 	save = data->a.len;
-	count = 0;
+	save_i = i;
 	while (i < data->a.len)
 	{
+		count = 0;
 		if (i <= data->a.len / 2)
 			count += i;
 		else
 			count += (i - data->a.len / 2);
 		count += desc_sort_count(data->a.arr[i], &data->b); //stk b
 		if (count < save)
+		{
 			save = count;
+			save_i = i;
+		}
+		/* printf("%d\n", count); */
 		i++;
 	}
-	printf("save: %d\n", save);
-	//save = min_ops
-	//reuse only 1 iteration to apply save on elem
+	return (save_i);
 }
 
-void	turk_sort(t_data *data) //a, b stacks
+void	desc_sort(int cheapest_index, t_stack *stk) //*stk = b
 {
-	if (data->a.len > 3) //len-- each time push to b
+	int	cheapest_elem;
+
+	cheapest_elem = stk->arr[cheapest_index];
+	while (stk->arr[0] != cheapest_elem)
+	{
+		if (cheapest_index < (stk->len / 2))
+			rotate(stk);
+		else
+			rev_rotate(stk);
+	}
+}
+
+//cheapest index(a), desc_sort(b), push(a->b)
+//continue... until a->len = 3
+void	a_to_b_sort(t_data *data)
+{
+	int i;
+
+	i = cheapest_index(data);
+	desc_sort(i, &data->b);
+	push(&data->a, &data->b);
+}
+
+void	turk_sort(t_data *data)
+{
+	if (data->a.len > 3)
 	{
 		push(&data->a, &data->b);
 		push(&data->a, &data->b);
-		/* while (data->a.len > 3) */
-			cheap_sort(data); //counter
+		while (data->a.len > 3)
+			a_to_b_sort(data);
 	}
 	if (data->a.len == 3)
 		three_sort(&data->a);
