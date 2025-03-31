965dec76319caab175a00a8da9ad1b2782b9044d
Paul Mallet <pamallet@c1r6p16.42lehavre.fr>
2025-03-31 18:27:35 +0200

    added infos

--- philosopher/philo/readme.txt
+++ philosopher/philo/readme.txt
@@ -10,4 +10,41 @@ Way to solve Philosophers
 * check if not died at every step
 * mutex monitor to see all actions of philos
 
+parsing like push_swap -> int, no negative value, handle 0
+
 nb_philos(also forks) ttdie(ms) tteat ttsleep nbtimes/philo_eat
+
+threads exec at same time (env 10ms interval)
+when fork -> duplicate env, syst ressources, global vars...
+when thread -> share all ressources, cause n thread / 1 process
+share memories -> can modif same vars = race conditions
+
+p_thread monitor
+	-> create + join independantly from philos
+	-> access to value
+	-> boolean as finished
+
+while philos(n from param)
+
+fsanitize -> detect data race, compile with objs + bin
+philo visualizer -> schema of prints return
+
+OS context switch and temp change thread access to data which is shared
+thread -> 
+mutex -> 
+race condition -> when 2 threads access to same address mem(var)
+deadlock -> mutex not unlock, process 1 wait for resource 2, process 2 wait for resource 1 etc.
+lock order inversion
+-> t1 acquires mutex A, tries to acquire mutex B
+-> t2 acquires mutex B, tries to acquire mutex A
+
+odd / even philos = threads
+	odd = pair = take right fork 1rst
+		ex: 4 philos -> 1(1) | 1(1) | 1(1) | 1(1)
+	even = impair = take left fork 1rst
+		ex: 5 philos -> 1(1) | 1(1) | 1(1) | 1(1) | 1(1)
+
+1 philo take 1rst fork on left, then right, eat, drop right, so another philo can take this drop one
+
+400 200 200 -> not enough time to take fork or doing action, so will die
+410 200 200 -> enough time
--- philosopher/philo/src/main.c
+++ philosopher/philo/src/main.c
@@ -6,13 +6,62 @@
 /*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 16:05:15 by pamallet          #+#    #+#             */
-/*   Updated: 2025/03/28 16:06:36 by pamallet         ###   ########.fr       */
+/*   Updated: 2025/03/31 15:53:11 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
+int	mails = 0;
+
+void	*routine()
+{
+	//printf "timestamp_in_ms X [action based on routine call]"
+	sleep(3);
+	for (int i = 0; i < 1000000; i++) {	//choose a big number to let OS context switch
+		mails++;	//3 CPU's operations(read, increment, write)*
+	}
+	printf("Ending thread");
+}
+
+//* quantum time reached, OS interrupt it to let another one exec
+
+// void	*routine2()
+// {
+// 	sleep(3);  
+// 	printf("Ending thread");
+// }
+
 int	main(void)
 {
+	pthread_t		t1, t2;
+
+	//fill 8 chars starting from str[13] with '.'
+	//memset("A string with many chars."[13], '.', 8 * sizeof(char));
+
+	//CREATE = start exec in calling process, by exec routine()
+	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+		return (EXIT_FAILURE);
+	pthread_create(&t2, NULL, &routine, NULL);
+
+	//JOIN = wait for the ending of the target thread
+	if (pthread_join(t1, NULL) != 0)
+		return (EXIT_FAILURE);
+	pthread_join(t2, NULL);
+
+	//suspend thread exec
+	//actual suspension time may be longer than the amount of time spe
+	//(cause other activity / time to spent process call)
+	unsigned int	usecs;
+
+	usecs = 1500;
+	usleep(usecs); //0(success) or -1(error)
+
+	//for timestamp_in_ms, give n of microsec tv.tv_usec since Epoch(time)
+	//if either tv or tz = NULL
+	// struct timeval	*tv;
+	// struct timezone	*tz; //?
+
+	// gettimeofday(tv, tz);
 	return (0);
 }
