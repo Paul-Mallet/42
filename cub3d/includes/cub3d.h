62156818b1172e8e194cc5112d1ab7c07bb407d9
Paul Mallet <pamallet@c1r6p16.42lehavre.fr>
2025-06-11 17:45:47 +0200

    added makefile setup with math.h and mlx

---
+++ cub3d/includes/cub3d.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 17:41:16 by pamallet          #+#    #+#             */
+/*   Updated: 2025/06/11 17:42:25 by pamallet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/time.h>
+# include <limits.h>
+# include <float.h>
+# include <math.h>
+# include "../minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/Xlib.h>
+# include <X11/keysym.h>
+
+//macros, enums, structs, functions...
+
+#endif
--- cub3d/src/main.c
+++ cub3d/src/main.c
@@ -6,35 +6,34 @@
 /*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 17:27:54 by pamallet          #+#    #+#             */
-/*   Updated: 2025/06/11 14:59:20 by pamallet         ###   ########.fr       */
+/*   Updated: 2025/06/11 17:45:04 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <limits.h>
-#include <float.h>
-#include <math.h>
-#include "minilibx-linux/mlx.h"
-#include <X11/X.h>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
+#include "cub3d.h"
+
+void	mlx_screen()
+{
+	//...
+}
 
 int	main(void)
 {
 	//float = float_t = FLT_EVAL_METHOD 0 = 4 bytes = 32 bits
 	//double = double_t = FLT_EVAL_METHOD 0 = 8 bytes = 64 bits
 	float	flt = 4.2;
+	double	dble = 4.2;
 	float_t	fltt1 = 4.2;
 	double_t dblet1 = 4.2;
 
+	/* FLOAT & DOUBLE */
 	printf("FLT_EVAL_METHOD: %d\n", __FLT_EVAL_METHOD__);
 	printf("float: %f sizeof(%ld) min(%f)\n", flt, sizeof(flt), FLT_MIN);
 	printf("float_t1: %f sizeof(%ld) max(%f)\n", fltt1, sizeof(fltt1), FLT_MAX);
+	printf("double: %f sizeof(%ld) min(%f)\n", dble, sizeof(dble), DBL_MIN);
 	printf("double_t1: %f sizeof(%ld) max(%f)\n", dblet1, sizeof(dblet1), DBL_MAX);
+	
+	/* MATH */
+	printf("acos: %f\n", acos(dble));
 	return (0);
 }
