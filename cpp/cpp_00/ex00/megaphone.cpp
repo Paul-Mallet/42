fe7c9e5c64a6cdc19f2856114020acc2edaf69cf
Paul Mallet <pamallet@c2r2p5.42lehavre.fr>
2025-07-30 18:04:16 +0200

    added basic headers installation

--- cpp/cpp_00/ex00/megaphone.cpp
+++ cpp/cpp_00/ex00/megaphone.cpp
@@ -6,12 +6,12 @@
 /*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 16:13:21 by pamallet          #+#    #+#             */
-/*   Updated: 2025/07/30 18:00:45 by pamallet         ###   ########.fr       */
+/*   Updated: 2025/07/30 18:03:05 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream> //iostream
-#include <cctype> //iostream
+#include <cctype> //char approach with toupper
 using namespace std; //namespace
 
 int	main(int ac, char **av)
@@ -22,7 +22,7 @@ int	main(int ac, char **av)
 	if (ac > 1)
 	{
 		while (av[i])
-			//string + transform
+			//string + transform, work seamlessly together(as standard)
 		cout << "\n";
 	}
 	else
