bd75b9cbea020da5eec282bd7704658902692935
paul_mallet <paulhavre42@gmail.com>
2025-07-31 15:33:25 +0200

    added megaphone exercise

--- cpp/cpp_00/ex00/megaphone.cpp
+++ cpp/cpp_00/ex00/megaphone.cpp
@@ -3,28 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paul_mallet <paul_mallet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 16:13:21 by pamallet          #+#    #+#             */
-/*   Updated: 2025/07/30 18:03:05 by pamallet         ###   ########.fr       */
+/*   Updated: 2025/07/31 15:32:36 by paul_mallet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream> //iostream
 #include <cctype> //char approach with toupper
-using namespace std; //namespace
+using std::cout;
+
+//namespace not good, don't know if my cout or the std::cout
+//string + transform, work seamlessly together(as standard)
 
 int	main(int ac, char **av)
 {
 	int	i;
+	int	j;
 
-	i = 1;
+	i = 0;
 	if (ac > 1)
 	{
-		while (av[i])
-			//string + transform, work seamlessly together(as standard)
+		while (av[++i])
+		{
+			j = -1;
+			while (av[i][++j])
+				av[i][j] = toupper(av[i][j]);
+			cout << av[i];
+		}
 		cout << "\n";
 	}
 	else
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
 }
