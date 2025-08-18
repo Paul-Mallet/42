9c20e24add643da01e6f9561e729e764a7d6b4f5
Paul Mallet <pamallet@c1r6p15.42lehavre.fr>
2025-08-18 16:39:39 +0200

    added ex06 with switch implementation

--- cpp/cpp_01/ex06/src/Harl.cpp
+++ cpp/cpp_01/ex06/src/Harl.cpp
@@ -6,50 +6,70 @@
 /*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 10:23:36 by paul_mallet       #+#    #+#             */
-/*   Updated: 2025/08/18 15:04:48 by pamallet         ###   ########.fr       */
+/*   Updated: 2025/08/18 16:32:18 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Harl.hpp"
 
-Harl::Harl( void ) {
-	std::cout << "Harl created.\n";
-}
+Harl::Harl( void ) {}
 
-Harl::~Harl() {
-	std::cout << "Harl destroyed.\n";	
-}
+Harl::~Harl() {}
 
 /**
- * void (Harl::*func)() = NULL; -> ptr to func address init to null, waiting for an address
- * scope & accessibility of member functions addresses:
- * 		(RECOMMANDED) func = &Harl::debug; -> access to private mem_fun from "Harl::" class, accessible from everywhere (in/outside)
- * 		(AVOID) func = &debug; -> access only WITHIN a member func of class, here complain already a mem of Harl so it's good
- * (this->*func)() -> deref the ptr func by calling the mem_fun it is pointing to
+ * switch keyword:
+ * 		ONLY take integral(int, char, bool) as param for perf opti(not like JS which can take whole string)
+ * 		break ; in case = stop exec rest of switch
+ * 		default: if none of cases validated do something else
  */
-//string array["WARNING", ...], index based;
 void Harl::complain( std::string level ) {
-	//...
+	std::string strings[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+	int i = 0;
+
+	while (i < 4)
+	{
+		if (level == strings[i])
+			break ;
+		i++;
+	}
+
+	switch(i) {
+		case 0:
+			(this->Harl::debug)();
+			std::cout << std::endl;
+		case 1:
+			(this->Harl::info)();
+			std::cout << std::endl;
+		case 2:
+			(this->Harl::warning)();
+			std::cout << std::endl;
+		case 3:
+			(this->Harl::error)();
+			std::cout << std::endl;
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]\n";
+	}
 }
 
 void Harl::debug( void ) {
-	std::cout << "[DEBUG]\nI love having extra bacon for my"
+	std::cout << "[ DEBUG ]\nI love having extra bacon for my"
 		<< " 7XL-double-cheese-triple-pickle-specialketchup burger."
-		<< " I really do!\n";
+		<< "\nI really do!\n";
 }
 
 void Harl::info( void ) {
-	std::cout << "[INFO]\nI cannot believe adding extra bacon costs more money."
+	std::cout << "[ INFO ]\nI cannot believe adding extra bacon costs more money."
 		<< "\nYou didn’t put enough bacon in my burger! If you did,"
 		<< " I wouldn’t be asking for more!\n";
 }
 
 void Harl::warning( void ) {
-	std::cout << "[WARNING]\nI think I deserve to have some extra bacon for free."
+	std::cout << "[ WARNING ]\nI think I deserve to have some extra bacon for free."
 		<< "\nI’ve been coming for years, whereas you started working"
 		<< " here just last month.\n";
 }
 
 void Harl::error( void ) {
-	std::cout << "[ERROR]\nThis is unacceptable! I want to speak to the manager now.\n";
+	std::cout << "[ ERROR ]\nThis is unacceptable! I want to speak to the manager now.\n";
 }
--- cpp/cpp_01/ex06/src/main.cpp
+++ cpp/cpp_01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 14:38:08 by pamallet          #+#    #+#             */
-/*   Updated: 2025/08/18 14:48:41 by pamallet         ###   ########.fr       */
+/*   Updated: 2025/08/18 16:23:22 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,15 @@
 // ./harlFilter "WARNING" || ./harlFilter "other than level" -> return same string
 int main ( int ac, char **av )
 {
+	Harl harl2_0;
+
 	if (ac != 2)
 	{
 		std::cout << "Valid format: ./harlFilter \"WARNING\"\n";
 		return 0;
 	}
 
-	Harl harl2_0;
-	Harl *harl2_00 = new Harl();
-
 	harl2_0.complain(av[1]);
-	harl2_00->complain(av[1]);
-
-	delete harl2_00;
+	
 	return 0;
 }
