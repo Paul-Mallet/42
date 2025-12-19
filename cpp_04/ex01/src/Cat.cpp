f7a998c9d83ee68d947f817c111399378b9cdde9
paul_mallet <paumallet76@gmail.com>
2025-12-19 14:45:49 +0100

    added main loops setup, still need to implement Dog to avoid delete seg fault

--- cpp_04/ex01/src/Cat.cpp
+++ cpp_04/ex01/src/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: paul_mallet <paul_mallet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 18:46:07 by paul_mallet       #+#    #+#             */
-/*   Updated: 2025/12/18 18:47:41 by paul_mallet      ###   ########.fr       */
+/*   Updated: 2025/12/19 14:27:44 by paul_mallet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 Cat::Cat( void ) : Animal()
 {
 	const std::string ttype = ("Cat");
-
+	
 	std::cout << "Cat: Default Constructor" << std::endl;
 	this->type = ttype;
+	this->_brain = new Brain();
 }
 
 Cat::Cat( std::string type ) : Animal()
@@ -26,10 +27,9 @@ Cat::Cat( std::string type ) : Animal()
 
 	std::cout << "Cat: Parameterized Constructor" << std::endl;
 
+	this->_brain = new Brain();
 	if (ttype.compare(type) == 0)
-	{
 		this->type = type;
-	}
 	else
 	{
 		std::cout << "Cat: Type is not a Cat" << std::endl;
@@ -43,19 +43,26 @@ Cat::Cat( Cat const &src ) : Animal()
 	std::cout << "Cat: Copy Constructor" << std::endl;
 
 	if (this != &src)
+	{
+		this->_brain = src._brain;
 		this->type = src.type;
+	}
 }
 
 Cat::~Cat( void )
 {
 	std::cout << "Cat: Destructor" << std::endl;
+	delete this->_brain;
 }
 
 Cat &Cat::operator=( Cat const &rhs )
 {
 	std::cout << "Cat: Copy Operator Assignment" << std::endl;
 	if (this != &rhs)
+	{
+		this->_brain = rhs._brain;
 		this->type = rhs.type;
+	}
 	return (*this);
 }
 
--- cpp_04/ex01/src/main.cpp
+++ cpp_04/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: paul_mallet <paul_mallet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 17:21:25 by pamallet          #+#    #+#             */
-/*   Updated: 2025/12/19 12:39:59 by paul_mallet      ###   ########.fr       */
+/*   Updated: 2025/12/19 14:44:37 by paul_mallet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,81 +25,33 @@ int	main(int ac, char **av)
 		return (1);
 	}
 
-	const Animal* animal = new Animal();
-	const Animal* animalv2 = new Animal(av[1]);
-	const Animal* animalv3 = animal;
-	const Animal* cat = new Cat();
-	const Animal* catv2 = new Cat(av[1]);
-	const Animal* catv3 = cat;
-	const Animal* dog = new Dog();
-	const Animal* dogv2 = new Dog(av[1]);
-	const Animal* dogv3 = dog;
-	const WrongAnimal* wrong_animal = new WrongAnimal();
-	const WrongAnimal* wrong_cat = new WrongCat();
+	const Animal* animals[6];
 
 	std::cout << std::endl;
 
-	animal->getType();
-	animalv2->getType();
-	animalv3->getType();
+	for (int i = 0; i < 3; i++)
+		animals[i++] = new Cat();
+	for (int i = 3; i < 6; i++)
+		animals[i++] = new Dog();
 
 	std::cout << std::endl;
 
-	std::cout << animal->getType() << std::endl;
-	std::cout << animalv2->getType() << std::endl;
-	std::cout << animalv3->getType() << std::endl;
+	animals[2]->getType();
+	animals[3]->getType();
 
 	std::cout << std::endl;
 
-	animal->makeSound();
-	animalv2->makeSound();
-	animalv3->makeSound();
+	std::cout << animals[2]->getType() << std::endl;
+	std::cout << animals[3]->getType() << std::endl;
 
 	std::cout << std::endl;
 
-	dog->getType();
-	dogv2->getType();
-	dogv3->getType();
+	animals[2]->makeSound();
+	animals[3]->makeSound();
 
 	std::cout << std::endl;
 
-	dog->makeSound();
-	dogv2->makeSound();
-	dogv3->makeSound();
-
-	std::cout << std::endl;
-
-	cat->getType();
-	catv2->getType();
-	catv3->getType();
-
-	std::cout << std::endl;
-
-	cat->makeSound();
-	catv2->makeSound();
-	catv3->makeSound();
-
-	std::cout << std::endl;
-
-	wrong_cat->getType();
-	wrong_cat->getType();
-	wrong_cat->getType();
-
-	std::cout << std::endl;
-
-	wrong_cat->makeSound();
-	wrong_cat->makeSound();
-	wrong_cat->makeSound();
-
-	std::cout << std::endl;
-
-	delete animal;
-	delete animalv2;
-	delete dog;
-	delete dogv2;
-	delete cat;
-	delete catv2;
-	delete wrong_animal;
-	delete wrong_cat;
+	for (int i = 0; i < 6; i++)
+		delete animals[i];
 	return (0);
 }
