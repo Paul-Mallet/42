43b4c5035b89feb3d73ad31a4d94c0b9793631cf
paul_mallet <paumallet76@gmail.com>
2026-01-29 09:59:03 +0100

    added explicit error on compilation message

--- cpp/cpp_06/ex01/src/main.cpp
+++ cpp/cpp_06/ex01/src/main.cpp
@@ -6,17 +6,18 @@
 /*   By: paul_mallet <paul_mallet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:05:01 by paul_mallet       #+#    #+#             */
-/*   Updated: 2026/01/19 10:12:32 by paul_mallet      ###   ########.fr       */
+/*   Updated: 2026/01/29 09:58:36 by paul_mallet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/Serializer.hpp"
-#include "../includes/Data.hpp"
+#include "Serializer.hpp"
 
 int	main( int ac, char **av )
 {
-	if (ac != 1)
+	if (ac != 1) {
+		std::cout << "Too many args\nex: ./serializer" << std::endl;
 		return (1);
+	}
 	(void)av;
 
 	Data*		ptr = new Data;
@@ -29,7 +30,8 @@ int	main( int ac, char **av )
 	std::cout << "ptr->i: " << ptr->i << std::endl;
 	std::cout << "ptr->f: " << ptr->f << std::endl;
 	std::cout << "ptr->str: " << ptr->str << std::endl;
-	std::cout << "raw: " << raw << std::endl;
+
+	std::cout << std::endl;
 
 	std::cout << "ptr: " << ptr << std::endl;
 	raw = Serializer::serialize(ptr);
@@ -37,8 +39,8 @@ int	main( int ac, char **av )
 	std::cout << "raw: " << raw << std::endl;
 	ptr = Serializer::deserialize(raw);
 
-	std::cout << "ptr: " << ptr << std::endl;
-	std::cout << "raw: " << raw << std::endl;
+	std::cout << std::endl;
+
 	std::cout << "ptr->i: " << ptr->i << std::endl;
 	std::cout << "ptr->f: " << ptr->f << std::endl;
 	std::cout << "ptr->str: " << ptr->str << std::endl;
