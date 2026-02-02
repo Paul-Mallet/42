6807061950a8732cb36913f3a484fa7585a6de9d
Paul Mallet <pamallet@c1r7p6.42lehavre.fr>
2026-02-02 17:27:32 +0100

    added tests comments

--- cpp/cpp_06/ex01/src/main.cpp
+++ cpp/cpp_06/ex01/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paul_mallet <paul_mallet@student.42.fr>    +#+  +:+       +#+        */
+/*   By: pamallet <pamallet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:05:01 by paul_mallet       #+#    #+#             */
-/*   Updated: 2026/01/29 09:58:36 by paul_mallet      ###   ########.fr       */
+/*   Updated: 2026/02/02 17:26:27 by pamallet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,28 +23,36 @@ int	main( int ac, char **av )
 	Data*		ptr = new Data;
 	uintptr_t	raw = 0;
 
+	/**
+	 * @brief Serialization round-trip validation tests
+	 */
 	ptr->i = 42;
-	ptr->f = 42.0f;
+	ptr->f = -42.0f;
 	ptr->str = "pamallet";
 
-	std::cout << "ptr->i: " << ptr->i << std::endl;
-	std::cout << "ptr->f: " << ptr->f << std::endl;
-	std::cout << "ptr->str: " << ptr->str << std::endl;
-
-	std::cout << std::endl;
-
 	std::cout << "ptr: " << ptr << std::endl;
-	raw = Serializer::serialize(ptr);
-
-	std::cout << "raw: " << raw << std::endl;
-	ptr = Serializer::deserialize(raw);
-
-	std::cout << std::endl;
-
 	std::cout << "ptr->i: " << ptr->i << std::endl;
 	std::cout << "ptr->f: " << ptr->f << std::endl;
 	std::cout << "ptr->str: " << ptr->str << std::endl;
+	std::cout << std::endl;
 
+	/**
+	 * @brief Serialization serialize tests
+	 */
+	raw = Serializer::serialize(ptr);
+	std::cout << "raw: " << raw << std::endl;
+	std::cout << std::endl;
+
+	/**
+	 * @brief Serialization deserialize tests
+	 */
+	ptr = Serializer::deserialize(raw);
+	std::cout << "ptr: " << ptr << std::endl;
+	std::cout << "ptr->i: " << ptr->i << std::endl;
+	std::cout << "ptr->f: " << ptr->f << std::endl;
+	std::cout << "ptr->str: " << ptr->str << std::endl;
+	std::cout << std::endl;
+	
 	delete ptr;
 
 	return (0);
